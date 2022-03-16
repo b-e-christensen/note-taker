909e6c6ff993075ed2b6434caa8779ae0ead4dc5
Ben Christensen <ben.eric.christensen@gmail.com>
2022-03-15 21:09:47 -0600

    added functionality to be able to delete notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,6 @@ const router = require('express').Router()
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 
-
 router.get('/notes', (req, res) => {
     const data = require('../db/db.json')
     res.json(data)
@@ -25,5 +24,25 @@ router.post('/notes', (req, res) => {
     //end -- responds with new note entered
     res.json(newNote)
 })
+
+router.delete('/notes/:id', (req, res, next) => {
+    const dbJSON = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+    const noteID = req.params.id
+    const noteIndex = dbJSON.findIndex(x => x.id === noteID);
+    if (noteIndex > -1) { 
+        dbJSON.splice(noteIndex, 1)
+        fs.writeFile('./db/db.json', JSON.stringify(dbJSON), err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            console.info('Database file updated')
+        })
+        
+    }
+    document.location.reload()
+});
+
+
 
 module.exports = router
